5570b25c3f4e26c75a5d2532f1c7d1b393ca9b21
Guilherme Lopes de Araujo <guilherme.xh@hotmail.com>
2024-01-20 19:05:19 -0300

    feat: change implementation to use String and add a benchmark

---
+++ src/main/java/br/com/glojuara/BenchmarkApp.java
@@ -0,0 +1,9 @@
+package br.com.glojuara;
+
+public class BenchmarkApp {
+
+    public static void main(String[] args) throws Exception {
+        org.openjdk.jmh.Main.main(args);
+    }
+
+}
--- src/main/java/br/com/glojuara/Validator.java
+++ src/main/java/br/com/glojuara/Validator.java
@@ -1,34 +1,43 @@
 package br.com.glojuara;
 
+import org.openjdk.jmh.annotations.*;
+
 public class Validator {
 
-    public static boolean isValidCpf(String cpf) {
-        char[] chars = cpf.toCharArray();
-        return isValidFormat(chars) && isValidDigit(chars, 1) && isValidDigit(chars, 2);
+    @Benchmark
+    @Fork(value = 1, warmups = 2)
+    @Warmup(iterations = 5)
+    @BenchmarkMode(Mode.All)
+    public void init() {
+        isValidCpf("860.082.270-00");
     }
 
-    private static boolean isValidFormat(char[] cpf) {
+    public static boolean isValidCpf(String cpf) {
+        return isValidFormat(cpf) && isValidDigit(cpf, 1) && isValidDigit(cpf, 2);
+    }
+
+    private static boolean isValidFormat(String cpf) {
         int cpfSize = 14;
-        if (cpf == null || cpf.length != cpfSize) {
+        if (cpf == null || cpf.length() != cpfSize) {
             return false;
         }
-        return cpf[3] == '.' && cpf[7] == '.' && cpf[11] == '-';
+        return cpf.charAt(3) == '.' && cpf.charAt(7) == '.' && cpf.charAt(11) == '-';
     }
 
-    private static boolean isValidDigit(char[] cpf, int digit) {
+    private static boolean isValidDigit(String cpf, int digit) {
         int sum = 0;
         int multiplier = 9 + digit;
         int limit = digit == 1 ? 10 : 12;
         for (int index = 0; index <= limit; index++) {
             if(index == 3 || index == 7 || index == 11) continue;
-            sum += Character.getNumericValue(cpf[index]) * multiplier--;
+            sum += Character.getNumericValue(cpf.charAt(index)) * multiplier--;
         }
         int rest = sum % 11;
         int digitIndex = 11 + digit;
         if (rest < 2) {
-            return Character.getNumericValue(cpf[digitIndex]) == 0;
+            return Character.getNumericValue(cpf.charAt(digitIndex)) == 0;
         }
-        return Character.getNumericValue(cpf[digitIndex]) == 11 - rest;
+        return Character.getNumericValue(cpf.charAt(digitIndex)) == 11 - rest;
     }
 
 }
